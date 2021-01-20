4e5b010ae518aa561b6153404c43dadafc90503d
Anthony Magrene <ekfhalomaster@gmail.com>
2021-01-19 21:04:17 -0500

    Additional Functionality
    -Pull network information
    -DNS info
    -tcp metrics
    -corrected cronjobs file name.

--- extractor.sh
+++ extractor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 AUTHFILES=/var/log/auth.lo*
-
 KERNFILES=/var/log/kern.lo*
+
+ip addr show > ipaddr.txt
+ip link show > iplink.txt
+sudo ss -tulw > sstulw.txt
+sudo ss -tulwn > sstulwn.txt
 sudo lsof -i -P -n > lsof.txt
-sudo netstat > netstat.txt
+sudo netstat -tulpn > netstat.txt
 cat /etc/passwd > passwd.txt
 sudo journalctl -k > systemdJournal.txt
 systemctl list-units --type=service > systemctl.txt
@@ -13,9 +17,13 @@ sudo cat /etc/sudoers > sudoers.txt
 history > history.txt
 ls /usr/lib/systemd/system > usrsystemd.txt
 ls /etc/systemd/system > etcsystemd.txt
+cat /etc/resolv.conf > dns.txt
+sudo ifconfig > ifconfig.txt
+sudo ip tcp_metrics show > tcpmetrics.txt
+
 for user in $(cut -f1 -d: /etc/passwd); 
 do 
-	crontab -u $user -l >> crobjobs.txt 
+	crontab -u $user -l >> cronjobs.txt 
 done
 
 for f in $AUTHFILES
