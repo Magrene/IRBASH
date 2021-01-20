82ffe88f94b8f7d7b2c9024f267f901aea8726f0
Anthony Magrene <ekfhalomaster@gmail.com>
2021-01-19 19:31:13 -0500

    Functionality
    -Pull kernel log
    -ps aux
    -Group pull

--- extractor.sh
+++ extractor.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 AUTHFILES=/var/log/auth.lo*
+
+KERNFILES=/var/log/kern.lo*
 sudo lsof -i -P -n > lsof.txt
-netstat > netstat.txt
+sudo netstat > netstat.txt
 cat /etc/passwd > passwd.txt
-journalctl -k > systemdJournal.txt
+sudo journalctl -k > systemdJournal.txt
+systemctl list-units --type=service > systemctl.txt
+ps aux > psaux.txt
+cat /etc/group > group.txt
+sudo cat /etc/sudoers > sudoers.txt
+history > history.txt
+
 
 for f in $AUTHFILES
 do
 	cat $f >> ssh.txt
 done
+
+for f in $KERNFILES
+do
+	cat $f >> kern.txt
+done
+
+
